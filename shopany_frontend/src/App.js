ab00c21e77a08a054ad164bf7a390a6616c87187
abdurrahman-abdulhakeem <abdurrahmanola21@gmail.com>
2024-01-13 17:17:40 -0800

    initial

--- shopany_frontend/src/App.js
+++ shopany_frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' index element={<LoginScreen />}/>
         
+          
           <Route path='/login' element={<LoginScreen />}/>
         
           <Route path='/register' element={<RegisterScreen />}/>
